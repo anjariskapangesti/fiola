3874ff1af2e135c41f56fdcb99fd699bf83ce13a
dikiraha <kiiinugraha123@gmail.com>
2023-12-06 10:55:55 +0700

    alert nomor hp it mgr

--- app/Models/Alert.php
+++ app/Models/Alert.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alert extends Model
 {
-    protected $nohpitmgr = '082125008160';
-    // protected $nohpitmgr = '082260050066';
+    // protected $nohpitmgr = '082125008160';
+    protected $nohpitmgr = '082260050066';
 
     public function getNoHpItMgr()
     {
