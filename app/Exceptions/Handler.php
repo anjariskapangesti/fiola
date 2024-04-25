de0092cc3136e4e084a9912a548a78de1f3a3b87
dikiraha <kiiinugraha123@gmail.com>
2024-04-25 16:14:47 +0700

    push qa

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,19 +50,20 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        $text_error = '';
-        $code_error = $exception->getStatusCode();
-    
-        if ($this->isHttpException($exception)) {
-            if ($code_error == 403) {
-                $text_error = 'Not permission';
-            } elseif ($code_error == 404) {
-                $text_error = 'Not found';
-            } elseif ($code_error == 500) {
-                $text_error = 'Server error';
-            }
-        }
-    
-        return response()->view('errors.error', ['text_error' => $text_error, 'code_error' => $code_error], $code_error);
+        // $text_error = '';
+        // $code_error = $exception->getStatusCode();
+
+        // if ($this->isHttpException($exception)) {
+        //     if ($code_error == 403) {
+        //         $text_error = 'Not permission';
+        //     } elseif ($code_error == 404) {
+        //         $text_error = 'Not found';
+        //     } elseif ($code_error == 500) {
+        //         $text_error = 'Server error';
+        //     }
+
+        //     return response()->view('errors.error', ['text_error' => $text_error, 'code_error' => $code_error], $code_error);
+        // }
+        return parent::render($request, $exception);
     }
 }
